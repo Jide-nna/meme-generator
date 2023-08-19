917a2dfab8217fec162a7824cf156baf696a7950
Bakinson Babajide <babajidebakinson@gmail.com>
2023-08-19 23:14:59 +0100

    First commit

---
+++ meme-generator-app/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+ import Header from "./components/header"
+import Memes from "./components/memes"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Memes />
+        </div>
+    )
+}
